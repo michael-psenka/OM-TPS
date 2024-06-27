2ac3ae1de4622c6655a0fbeb6036196209cd2651
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2024-06-26 22:50:31 -0700

    update error checking on latent time

--- src/om_interpolation.py
+++ src/om_interpolation.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     ckpt_path = args.ckpt_path
     path_length = args.path_length
     latent_time = args.latent_time
-    if latent_time > 1000:
+    if latent_time >= 1000:
         raise ValueError(
             "latent_time must be less than or equal to total diffusion model time of 1000"
         )
