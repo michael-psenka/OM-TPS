7ed9fbd5d895204eee274cd73ad3bded2e0fdc23
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2024-07-08 10:34:35 -0700

    merge in hessian action definition, but not the optimization procedure

--- src/actions.py
+++ src/actions.py
@@ -59,18 +59,51 @@ class HessianAction(torch.nn.Module):
     because it is constant and does not affect the optimization.
     """
 
-    def __init__(self, dt_xi):
+    def __init__(self, dt, xi, D):
+        """
+        Args:
+            dt: float, time step
+            xi: float, inverse temperature
+            D: float, diffusion coefficient
+        """
         super(HessianAction, self).__init__()
-        self.dt_xi = dt_xi
+        self.dt = dt
+        self.xi = xi
+        self.D = D
 
-    def forward(self, path: torch.Tensor, forces: torch.Tensor):
+    def forward(
+        self, path: torch.Tensor, forces: torch.Tensor, force_grads: torch.Tensor
+    ):
         """
         Args:
             path: torch.Tensor of images of shape [P, C, H, W], where P is the number of images on the path.
             forces: torch.Tensor of diffusion model score estimates of shape [P, C, H, W], where N is the number of points on the path.
+            force_grads: torch.Tensor of gradients of the forces w.r.t. the path, of shape [P, C, H, W], where N is the number of points on the path.
+            We only use the diagonal elements of the force grads. That is, how does the image at each point affect the force at that same point.
+            We only use the diagonal elements of the force grads. That is, how does the image at each point affect the force at that same point.
         Returns the OM action of the path (torch.Tensor of shape [1]).
         """
-        raise NotImplementedError("HessianAction is not implemented yet.")
+        # assert path.shape == forces.shape, "path and forces must have the same shape"
+        assert len(path.shape) == 4, "path and forces must have shape [P, C, H, W]"
+        assert (
+            forces.shape == force_grads.shape
+        ), "forces and force_grads must have the same shape"
+
+        # add extra dimension to account for term-by-term case
+        if forces[0].shape[0] == 1:
+            forces = forces.repeat(2, 1, 1, 1)
+            force_grads = force_grads.repeat(2, 1, 1, 1)
+
+        first_term = torch.square(1 / self.dt * (path[1:] - path[:-1]))
+
+        second_term = torch.square(forces[:-1] / self.xi)
+
+        third_term = (
+            -2 * self.D / self.xi * force_grads[:-1]
+        )  # TODO: is there a minus sign in front of this term? (See Eqn 10 from https://pubs.aip.org/aip/jcp/article/132/13/134101/902772/Onsager-Machlup-action-based-path-sampling-and-its)
+        result = torch.sum(first_term + second_term + third_term)
+
+        return result * self.dt / 2
 
 
 """
