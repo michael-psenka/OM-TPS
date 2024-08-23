90cc42a807fc73666293329efdaa8874e6b4e4b9
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2024-08-23 16:55:45 -0700

    vary timestep expts

--- two-for-one-diffusion/models/ddpm.py
+++ two-for-one-diffusion/models/ddpm.py
@@ -462,7 +462,7 @@ class GaussianDiffusion(nn.Module):
                 action_func = action_cls(
                     force_func=force_func,
                     laplace_func=laplace,
-                    dt=1,
+                    dt=0.01,
                     gamma=12,
                     D=0.1,
                 )  # TODO: figure out dt, gamma, D
