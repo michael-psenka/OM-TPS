55c937dbb0f8eecebcc221c83800802810a93b4d
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2024-09-25 14:32:59 -0700

    add pwd and dihedral tracking

--- two-for-one-diffusion/sample.sh
+++ two-for-one-diffusion/sample.sh
@@ -34,15 +34,15 @@
 python sample.py \
     --model_path saved_models/bba \
     --gen_mode om_interpolate \
-    --num_samples_eval 8 \
-    --batch_size_gen 8 \
+    --num_samples_eval 2 \
+    --batch_size_gen 2 \
     --latent_time 20 \
     --initial_guess_level 250 \
     --no_encode_and_decode \
     --append_exp_name test_temp=1.5 \
     --path_length 200 \
     --steps 1000 \
-    --interpolation_temp 1.5
+    --interpolation_temp 1.0
 
 # python sample.py \
 #     --model_path saved_models/villin \
@@ -67,37 +67,5 @@ python sample.py \
 #     --path_length 200 \
 #     --steps 1000
 
-# CHIGNOLIN
-# python sample.py \
-#     --model_path saved_models/chignolin \
-#     --gen_mode langevin \
-#     --noise_level 20 \
-#     --parallel_sim 100 \
-#     --n_timesteps 6000000 \
-#     --n_timesteps 6000000 \
-#     --save_interval 500 \
-#     --kb consistent \
-#     --dt 2e-3
 
-# # TRP-CAGE
-# python sample.py \
-#     --model_path saved_models/trp_cage \
-#     --gen_mode langevin \
-#     --noise_level 15 \
-#     --parallel_sim 100 \
-#     --n_timesteps 6000000 \
-#     --save_interval 500 \
-#     --kb consistent \
-#     --dt 2e-3
-
-# # BBA
-# python sample.py \
-#     --model_path saved_models/bba \
-#     --gen_mode langevin \
-#     --noise_level 5 \
-#     --parallel_sim 100 \
-#     --n_timesteps 6000000 \
-#     --save_interval 500 \
-#     --kb consistent \
-#     --dt 2e-3
 
