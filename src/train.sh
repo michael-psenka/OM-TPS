12ee3866e9ef729e2a27fd08a91e2985d2a5f1d0
michael-psenka <michael.psenka@gmail.com>
2024-09-28 20:19:53 -0700

    prepare for merge

--- src/train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-f_scale_values=($(seq 0.2 -0.02 0.02))
-
-# Loop through each f_scale value and run the command
-for f_scale in "${f_scale_values[@]}"; do
-    echo "Running with --f_scale $f_scale"
-    python om_interpolation.py --disable_logging --batch_size 32 --max_pairs 32 --steps 2000 --f_scale "$f_scale"
-done
