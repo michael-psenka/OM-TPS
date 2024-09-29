cb758831d538882aa52cc9ca3722840a9b9c9204
michael-psenka <michael.psenka@gmail.com>
2024-09-28 20:20:39 -0700

    add celeba

--- src/data/dataloaders.py
+++ src/data/dataloaders.py
@@ -1,6 +1,9 @@
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchvision.datasets import MNIST, ImageNet
 from torchvision import transforms
+
+import os
+from PIL import Image
 
 
 def create_mnist_dataloaders(batch_size, image_size=28, num_workers=4):
@@ -49,3 +52,56 @@ def create_imagenet_dataloaders(batch_size, image_size=256, num_workers=4):
     ), DataLoader(
         test_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers
     )
+
+
+class CelebADataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        # Get all image file paths in the directory
+        self.image_paths = [
+            os.path.join(root_dir, fname)
+            for fname in os.listdir(root_dir)
+            if fname.endswith(".png")
+        ]
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        # Load the image
+        image = Image.open(img_path)
+
+        # Apply any transformations if provided
+        if self.transform:
+            image = self.transform(image)
+
+        # return blank label to conform
+        return image, 0
+
+
+def create_celeba_dataloaders(batch_size):
+    transform = transforms.Compose(
+        [
+            # transforms.Resize((256, 256)),  # Resize to 256x256 if necessary
+            transforms.ToTensor(),  # Convert image to tensor
+            transforms.Normalize([0.5], [0.5]),
+        ]
+    )
+
+    # Define the directories
+    train_dir = "/shared/mpsenka/celebahq256/train"
+    valid_dir = "/shared/mpsenka/celebahq256/valid"
+
+    # Create the train and validation datasets using the custom dataset
+    train_dataset = CelebADataset(train_dir, transform=transform)
+    valid_dataset = CelebADataset(valid_dir, transform=transform)
+
+    # Create DataLoaders
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    valid_loader = DataLoader(
+        valid_dataset, batch_size=batch_size, shuffle=False, num_workers=4
+    )
+
+    return train_loader, valid_loader
