b34cc2f87cd4abdd04f40ded22d4e695ed8e0e7d
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2025-01-15 21:55:45 -0800

    launch some tetrapeptide expts

--- two-for-one-diffusion/sample2.sh
+++ two-for-one-diffusion/sample2.sh
@@ -1,10 +1,32 @@
 python sample.py \
     --model_path saved_models/tetrapeptides \
     --gen_mode om_interpolate \
-    --tetra_seq AVGR \
     --data_folder /data/sanjeevr/4AA_sim \
-    --num_samples_eval  \
-    --batch_size_gen 16 \
+    --split mdgen/splits/4AA_test_small.csv \
+    --num_samples_eval 100  \
+    --batch_size_gen 100 \
+    --latent_time 0 \
+    --initial_guess_level 0 \
+    --subsample_points_percent 1.0 \
+    --subsample_dimensions_percent 1.0 \
+    --no_encode_and_decode \
+    --action truncated \
+    --optimizer adam \
+    --lr 2e-1 \
+    --append_exp_name initial_guess_level=0 \
+    --path_length 25 \
+    --om_dt 1 \
+    --om_d 0.1 \
+    --steps 250 \
+
+
+python sample.py \
+    --model_path saved_models/tetrapeptides \
+    --gen_mode om_interpolate \
+    --data_folder /data/sanjeevr/4AA_sim \
+    --split mdgen/splits/4AA_test_small.csv \
+    --num_samples_eval 100  \
+    --batch_size_gen 100 \
     --latent_time 0 \
     --initial_guess_level 100 \
     --subsample_points_percent 1.0 \
@@ -13,12 +35,55 @@ python sample.py \
     --action truncated \
     --optimizer adam \
     --lr 2e-1 \
-    --append_exp_name test \
+    --append_exp_name initial_guess_level=100 \
     --path_length 25 \
     --om_dt 1 \
     --om_d 0.1 \
     --steps 250 \
-    # --split mdgen/splits/4AA_test_small.csv \
+
+python sample.py \
+    --model_path saved_models/tetrapeptides \
+    --gen_mode interpolate \
+    --data_folder /data/sanjeevr/4AA_sim \
+    --split mdgen/splits/4AA_test_small.csv \
+    --num_samples_eval 100  \
+    --batch_size_gen 100 \
+    --latent_time 0 \
+    --initial_guess_level 100 \
+    --subsample_points_percent 1.0 \
+    --subsample_dimensions_percent 1.0 \
+    --no_encode_and_decode \
+    --action truncated \
+    --optimizer adam \
+    --lr 2e-1 \
+    --append_exp_name latent_time=0 \
+    --path_length 25 \
+    --om_dt 1 \
+    --om_d 0.1 \
+    --steps 250 \
+
+
+python sample.py \
+    --model_path saved_models/tetrapeptides \
+    --gen_mode interpolate \
+    --data_folder /data/sanjeevr/4AA_sim \
+    --split mdgen/splits/4AA_test_small.csv \
+    --num_samples_eval 100  \
+    --batch_size_gen 100 \
+    --latent_time 100 \
+    --initial_guess_level 100 \
+    --subsample_points_percent 1.0 \
+    --subsample_dimensions_percent 1.0 \
+    --no_encode_and_decode \
+    --action truncated \
+    --optimizer adam \
+    --lr 2e-1 \
+    --append_exp_name latent_time=100 \
+    --path_length 25 \
+    --om_dt 1 \
+    --om_d 0.1 \
+    --steps 250 \
+    
 
 
 # python sample.py \
