22d6d9704e883227be83dc08f42f12b60c708856
jeevster <sanjeevr@berkeley.edu>
2025-01-25 21:32:35 -0800

    looking over flow results

--- two-for-one-diffusion/sample.sh
+++ two-for-one-diffusion/sample.sh
@@ -1,5 +1,5 @@
 python sample.py \
-    --model_path saved_models/chignolin \
+    --model_path saved_models/trp_cage \
     --gen_mode om_interpolate \
     --flow_matching \
     --num_samples_eval 8 \
@@ -19,7 +19,7 @@ python sample.py \
     --steps 5000
 
 python sample.py \
-    --model_path saved_models/chignolin \
+    --model_path saved_models/trp_cage \
     --gen_mode om_interpolate \
     --flow_matching \
     --num_samples_eval 8 \
@@ -38,40 +38,40 @@ python sample.py \
     --om_d 0.1 \
     --steps 5000
 
-python sample.py \
-    --model_path saved_models/bba \
-    --gen_mode om_interpolate \
-    --flow_matching \
-    --num_samples_eval 8 \
-    --batch_size_gen 2 \
-    --latent_time 0.5 \
-    --initial_guess_level 7 \
-    --subsample_points_percent 1.0 \
-    --subsample_dimensions_percent 1.0 \
-    --no_encode_and_decode \
-    --action hutch \
-    --optimizer sgd \
-    --lr 1e-3 \
-    --append_exp_name test_sgd_hutch_initial_guess_level=7_dt=0.05 \
-    --om_dt 0.05 \
-    --path_length 200 \
-    --steps 5000
+# python sample.py \
+#     --model_path saved_models/bba \
+#     --gen_mode om_interpolate \
+#     --flow_matching \
+#     --num_samples_eval 8 \
+#     --batch_size_gen 2 \
+#     --latent_time 0.5 \
+#     --initial_guess_level 7 \
+#     --subsample_points_percent 1.0 \
+#     --subsample_dimensions_percent 1.0 \
+#     --no_encode_and_decode \
+#     --action hutch \
+#     --optimizer sgd \
+#     --lr 1e-3 \
+#     --append_exp_name test_sgd_hutch_initial_guess_level=7_dt=0.05 \
+#     --om_dt 0.05 \
+#     --path_length 200 \
+#     --steps 5000
 
-python sample.py \
-    --model_path saved_models/bba \
-    --gen_mode om_interpolate \
-    --flow_matching \
-    --num_samples_eval 32 \
-    --batch_size_gen 2 \
-    --latent_time 0.5 \
-    --initial_guess_level 7 \
-    --subsample_points_percent 1.0 \
-    --subsample_dimensions_percent 1.0 \
-    --no_encode_and_decode \
-    --action truncated \
-    --optimizer sgd \
-    --lr 1e-3 \
-    --append_exp_name test_sgd_initial_guess_level=7_dt=0.05_32paths \
-    --om_dt 0.05 \
-    --path_length 200 \
-    --steps 5000
+# python sample.py \
+#     --model_path saved_models/bba \
+#     --gen_mode om_interpolate \
+#     --flow_matching \
+#     --num_samples_eval 32 \
+#     --batch_size_gen 2 \
+#     --latent_time 0.5 \
+#     --initial_guess_level 7 \
+#     --subsample_points_percent 1.0 \
+#     --subsample_dimensions_percent 1.0 \
+#     --no_encode_and_decode \
+#     --action truncated \
+#     --optimizer sgd \
+#     --lr 1e-3 \
+#     --append_exp_name test_sgd_initial_guess_level=7_dt=0.05_32paths \
+#     --om_dt 0.05 \
+#     --path_length 200 \
+#     --steps 5000
