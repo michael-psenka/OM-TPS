35afcb5deac06aed522028c586170bb10f5ff823
jeevster <sanjeevr@berkeley.edu>
2025-01-25 18:57:38 -0800

    flow matching hp tuning

--- two-for-one-diffusion/sample.sh
+++ two-for-one-diffusion/sample.sh
@@ -1,48 +1,77 @@
-# python sample.py \
-#     --model_path saved_models/bba \
-#     --gen_mode om_interpolate \
-#     --transition_data_removed \
-#     --num_samples_eval 8 \
-#     --batch_size_gen 2 \
-#     --latent_time 20 \
-#     --initial_guess_level 250 \
-#     --subsample_points_percent 1.0 \
-#     --subsample_dimensions_percent 1.0 \
-#     --no_encode_and_decode \
-#     --action hutch \
-#     --optimizer sgd \
-#     --lr 1e-3 \
-#     --append_exp_name test \
-#     --path_length 200 \
-#     --om_d 0.01 \
-#     --steps 4000
+python sample.py \
+    --model_path saved_models/chignolin \
+    --gen_mode om_interpolate \
+    --flow_matching \
+    --num_samples_eval 8 \
+    --batch_size_gen 2 \
+    --latent_time 0.5 \
+    --initial_guess_level 7 \
+    --subsample_points_percent 1.0 \
+    --subsample_dimensions_percent 1.0 \
+    --no_encode_and_decode \
+    --action truncated \
+    --optimizer sgd \
+    --lr 1e-3 \
+    --append_exp_name test_sgd_initial_guess_level=7_dt=0.05 \
+    --om_dt 0.05 \
+    --path_length 200 \
+    --om_d 0.1 \
+    --steps 5000
+
+python sample.py \
+    --model_path saved_models/chignolin \
+    --gen_mode om_interpolate \
+    --flow_matching \
+    --num_samples_eval 8 \
+    --batch_size_gen 2 \
+    --latent_time 0.5 \
+    --initial_guess_level 7 \
+    --subsample_points_percent 1.0 \
+    --subsample_dimensions_percent 1.0 \
+    --no_encode_and_decode \
+    --action hutch \
+    --optimizer sgd \
+    --lr 1e-3 \
+    --append_exp_name test_sgd_hutch_initial_guess_level=7_dt=0.05 \
+    --om_dt 0.05 \
+    --path_length 200 \
+    --om_d 0.1 \
+    --steps 5000
 
 python sample.py \
     --model_path saved_models/bba \
-    --gen_mode langevin \
-    --noise_level 5 \
-    --parallel_sim 8 \
-    --n_timesteps 25000000 \
-    --save_interval 500 \
-    --append_exp_name 8_sims_50ns \
-    --kb consistent \
-    --dt 2e-3 
+    --gen_mode om_interpolate \
+    --flow_matching \
+    --num_samples_eval 8 \
+    --batch_size_gen 2 \
+    --latent_time 0.5 \
+    --initial_guess_level 7 \
+    --subsample_points_percent 1.0 \
+    --subsample_dimensions_percent 1.0 \
+    --no_encode_and_decode \
+    --action hutch \
+    --optimizer sgd \
+    --lr 1e-3 \
+    --append_exp_name test_sgd_hutch_initial_guess_level=7_dt=0.05 \
+    --om_dt 0.05 \
+    --path_length 200 \
+    --steps 5000
 
-# python sample.py \
-#     --model_path saved_models/chignolin \
-#     --gen_mode om_interpolate \
-#     --transition_data_removed \
-#     --num_samples_eval 8 \
-#     --batch_size_gen 2 \
-#     --latent_time 20 \
-#     --initial_guess_level 250 \
-#     --subsample_points_percent 1.0 \
-#     --subsample_dimensions_percent 1.0 \
-#     --no_encode_and_decode \
-#     --action truncated \
-#     --optimizer sgd \
-#     --lr 1e-3 \
-#     --append_exp_name test_initial_latent_time=250_SGD \
-#     --path_length 200 \
-#     --om_d 0.1 \
-#     --steps 2000
+python sample.py \
+    --model_path saved_models/bba \
+    --gen_mode om_interpolate \
+    --flow_matching \
+    --num_samples_eval 32 \
+    --batch_size_gen 2 \
+    --latent_time 0.5 \
+    --initial_guess_level 7 \
+    --subsample_points_percent 1.0 \
+    --subsample_dimensions_percent 1.0 \
+    --no_encode_and_decode \
+    --action truncated \
+    --optimizer sgd \
+    --lr 1e-3 \
+    --append_exp_name test_sgd_initial_guess_level=7_dt=0.05_32paths \
+    --om_dt 0.05 \
+    --path_length 200 \
+    --steps 5000
