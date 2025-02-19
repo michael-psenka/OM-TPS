df57f6113681658816bb7902188ba0c655566d45
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2025-02-18 16:31:06 -0800

    getting mb rates which reasonably match ground truth from fenics

--- mb_fem.py
+++ mb_fem.py
@@ -3,17 +3,31 @@ from fenics import *
 from mshr import *
 import matplotlib.pyplot as plt
 import numpy as np
+from mb_calculator import MullerBrownPotential
 
 if __name__ == "__main__":
     # Make domain specific to problem
     # react_min = np.genfromtxt('react_min.txt')
     # prod_min = np.genfromtxt('prod_min.txt')
-    react_min = np.array([-0.558, 1.442])
-    prod_min = np.array([0.623,0.028])
-    radii = 0.025
-    rectangle = Rectangle(Point(-1.75,-0.5), Point(1.25,2.25))
-    react_domain = Circle(Point(react_min[0],react_min[1]), radii) 
-    prod_domain = Circle(Point(prod_min[0],prod_min[1]), radii) 
+
+    beta = Constant('16.58') # inverse kB*T
+    react_radii = 5
+    prod_radii = 5
+    calculator = MullerBrownPotential(device='cpu')
+
+    react_min = np.array(calculator.initial_point)
+    prod_min = np.array(calculator.final_point)
+
+    A = np.array(calculator.A)
+    alpha = np.array(calculator.alpha)
+    beta_ = np.array(calculator.beta)
+    gamma = np.array(calculator.gamma)
+    x_ref = np.array(calculator.a)
+    y_ref = np.array(calculator.b)
+
+    rectangle = Rectangle(Point(calculator.Lx, calculator.Ly), Point(calculator.Hx, calculator.Hy))
+    react_domain = Circle(Point(react_min[0],react_min[1]), react_radii) 
+    prod_domain = Circle(Point(prod_min[0],prod_min[1]), prod_radii) 
     domain = rectangle
 
     # Make subdomains
@@ -52,32 +66,57 @@ if __name__ == "__main__":
     # Define variational problem
     u = TrialFunction(V)
     v = TestFunction(V)
-    beta = Constant('1')
-    f_0 = Expression('A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=-20, a_=-1, b_=0, c_=-10, x_=1, y_=0)
-    f_0_exp = Expression('exp(-beta*A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_)))', degree=2, A_=-20, a_=-1, b_=0, c_=-10, x_=1, y_=0, beta=1)
-    f_0_x = Expression('A_*(2*a_*(x[0]-x_)+b_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=-20, a_=-1, b_=0, c_=-10, x_=1, y_=0)
-    f_0_y = Expression('A_*(b_*(x[0]-x_)+2*c_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=-20, a_=-1, b_=0, c_=-10, x_=1, y_=0)
-    f_0_grad = as_vector((f_0_x,f_0_y))
-    f_1 = Expression('A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=-10, a_=-1, b_=0, c_=-10, x_=0, y_=0.5)
-    f_1_exp = Expression('exp(-beta*A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_)))', degree=2, A_=-10, a_=-1, b_=0, c_=-10, x_=0, y_=0.5, beta=1)
-    f_1_x = Expression('A_*(2*a_*(x[0]-x_)+b_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=-10, a_=-1, b_=0, c_=-10, x_=0, y_=0.5)
-    f_1_y = Expression('A_*(b_*(x[0]-x_)+2*c_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=-10, a_=-1, b_=0, c_=-10, x_=0, y_=0.5)
-    f_1_grad = as_vector((f_1_x,f_1_y))
-    f_2 = Expression('A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=-17, a_=-6.5, b_=11, c_=-6.5, x_=-0.5, y_=1.5)
-    f_2_exp = Expression('exp(-beta*A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_)))', degree=2, A_=-17, a_=-6.5, b_=11, c_=-6.5, x_=-0.5, y_=1.5, beta=1)
-    f_2_x = Expression('A_*(2*a_*(x[0]-x_)+b_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=-17, a_=-6.5, b_=11, c_=-6.5, x_=-0.5, y_=1.5)
-    f_2_y = Expression('A_*(b_*(x[0]-x_)+2*c_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=-17, a_=-6.5, b_=11, c_=-6.5, x_=-0.5, y_=1.5)
-    f_2_grad = as_vector((f_2_x,f_2_y))
-    f_3 = Expression('A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=1.5, a_=0.7, b_=0.6, c_=0.7, x_=-1, y_=1)
-    f_3_exp = Expression('exp(-beta*A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_)))', degree=2, A_=1.5, a_=0.7, b_=0.6, c_=0.7, x_=-1, y_=1, beta=1)
-    f_3_x = Expression('A_*(2*a_*(x[0]-x_)+b_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=1.5, a_=0.7, b_=0.6, c_=0.7, x_=-1, y_=1)
-    f_3_y = Expression('A_*(b_*(x[0]-x_)+2*c_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', degree=2, A_=1.5, a_=0.7, b_=0.6, c_=0.7, x_=-1, y_=1)
-    f_3_grad = as_vector((f_3_x,f_3_y))
+    
+
+    f_0 = Expression('A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                 degree=2, A_=A[0], a_=alpha[0], b_=beta_[0], c_=gamma[0], x_=x_ref[0], y_=y_ref[0])
+    f_0_exp = Expression('exp(-beta*A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_)))', 
+                        degree=2, A_=A[0], a_=alpha[0], b_=beta_[0], c_=gamma[0], x_=x_ref[0], y_=y_ref[0], beta=beta.values().item())
+    f_0_x = Expression('A_*(2*a_*(x[0]-x_)+b_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[0], a_=alpha[0], b_=beta_[0], c_=gamma[0], x_=x_ref[0], y_=y_ref[0])
+    f_0_y = Expression('A_*(b_*(x[0]-x_)+2*c_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[0], a_=alpha[0], b_=beta_[0], c_=gamma[0], x_=x_ref[0], y_=y_ref[0])
+    f_0_grad = as_vector((f_0_x, f_0_y))
+
+    # Repeat for f_1
+    f_1 = Expression('A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[1], a_=alpha[1], b_=beta_[1], c_=gamma[1], x_=x_ref[1], y_=y_ref[1])
+    f_1_exp = Expression('exp(-beta*A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_)))', 
+                        degree=2, A_=A[1], a_=alpha[1], b_=beta_[1], c_=gamma[1], x_=x_ref[1], y_=y_ref[1], beta=beta.values().item())
+    f_1_x = Expression('A_*(2*a_*(x[0]-x_)+b_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[1], a_=alpha[1], b_=beta_[1], c_=gamma[1], x_=x_ref[1], y_=y_ref[1])
+    f_1_y = Expression('A_*(b_*(x[0]-x_)+2*c_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[1], a_=alpha[1], b_=beta_[1], c_=gamma[1], x_=x_ref[1], y_=y_ref[1])
+    f_1_grad = as_vector((f_1_x, f_1_y))
+
+    # Repeat for f_2
+    f_2 = Expression('A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[2], a_=alpha[2], b_=beta_[2], c_=gamma[2], x_=x_ref[2], y_=y_ref[2])
+    f_2_exp = Expression('exp(-beta*A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_)))', 
+                        degree=2, A_=A[2], a_=alpha[2], b_=beta_[2], c_=gamma[2], x_=x_ref[2], y_=y_ref[2], beta=beta.values().item())
+    f_2_x = Expression('A_*(2*a_*(x[0]-x_)+b_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[2], a_=alpha[2], b_=beta_[2], c_=gamma[2], x_=x_ref[2], y_=y_ref[2])
+    f_2_y = Expression('A_*(b_*(x[0]-x_)+2*c_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[2], a_=alpha[2], b_=beta_[2], c_=gamma[2], x_=x_ref[2], y_=y_ref[2])
+    f_2_grad = as_vector((f_2_x, f_2_y))
+
+    # Repeat for f_3
+    f_3 = Expression('A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[3], a_=alpha[3], b_=beta_[3], c_=gamma[3], x_=x_ref[3], y_=y_ref[3])
+    f_3_exp = Expression('exp(-beta*A_*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_)))', 
+                        degree=2, A_=A[3], a_=alpha[3], b_=beta_[3], c_=gamma[3], x_=x_ref[3], y_=y_ref[3], beta=beta.values().item())
+    f_3_x = Expression('A_*(2*a_*(x[0]-x_)+b_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[3], a_=alpha[3], b_=beta_[3], c_=gamma[3], x_=x_ref[3], y_=y_ref[3])
+    f_3_y = Expression('A_*(b_*(x[0]-x_)+2*c_*(x[1]-y_))*exp(a_*(x[0]-x_)*(x[0]-x_)+b_*(x[0]-x_)*(x[1]-y_)+c_*(x[1]-y_)*(x[1]-y_))', 
+                    degree=2, A_=A[3], a_=alpha[3], b_=beta_[3], c_=gamma[3], x_=x_ref[3], y_=y_ref[3])
+    f_3_grad = as_vector((f_3_x, f_3_y))
+
+
+    
     f_total = f_0+f_1+f_2+f_3
     f_total_exp = f_0_exp*f_1_exp*f_2_exp*f_3_exp
     f_grad_total = f_0_grad+f_1_grad+f_2_grad+f_3_grad
     #f_total = Expression('10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)', degree=2)
-    beta = Constant('1')
     a = (dot(grad(u), grad(v))+beta*dot(f_grad_total,grad(u))*v)*dx
     L = Constant('0')*v*dx
 
@@ -91,14 +130,13 @@ if __name__ == "__main__":
     np.savetxt("vertex_coords.txt", coordinates)
 
     # Evaluate u at points
-    p = Point(-0.6,0.5)
+    # p = Point(-0.6,0.5)
     
     gu = grad(u)
     Gu = project(gu, solver_type="cg")
     #Gu = dot(Gu,Gu)
     cost = assemble(dot(grad(u), grad(u))*f_total_exp*dx)
-    print(cost)
-
+    # print(cost)
 
     # Save solution to file in VTK format
     vtkfile = File('poisson/solution.pvd')
@@ -106,8 +144,8 @@ if __name__ == "__main__":
 
     # Evaluate points on structured grid
     n_struct = 100
-    points_x = np.linspace(-1.75,1.25,n_struct)
-    points_y = np.linspace(-0.5,2.25,n_struct)
+    points_x = np.linspace(calculator.Lx,calculator.Hx,n_struct)
+    points_y = np.linspace(calculator.Ly,calculator.Hy,n_struct)
     xx, yy = np.meshgrid(points_x,points_y)
     zz = np.zeros_like(xx)
     grad_2_zz = np.zeros_like(xx)
@@ -133,44 +171,35 @@ if __name__ == "__main__":
             for j in range(n_struct):
                 outf.write("{:.6g} {:.6g}\n".format(xx[i][j],yy[i][j]))
 
-    A = np.array([-20,-10,-17,1.5])
-    a = np.array([-1,-1,-6.5,0.7])
-    b = np.array([0,0,11,0.6])
-    c = np.array([-10,-10,-6.5,0.7])
-    x_ = np.array([1,0,-0.5,-1])
-    y_ = np.array([0,0.5,1.5,1])
-
-    def energy(x,y,A,a,b,c,x_,y_):
-        energy_ = np.zeros((x.shape))
-        for i in range(len(A)):
-            energy_ += A[i]*np.exp(a[i]*(x-x_[i])**2+b[i]*(x-x_[i])*(y-y_[i])+c[i]*(y-y_[i])**2)
-        return energy_
-
-    energies = energy(xx,yy,A,a,b,c,x_,y_)
+    # Evaluate energies over grid
+    grid_points = np.stack([xx, yy], axis = -1).reshape(-1, 2)
+    energies = calculator.get_energy(grid_points).reshape(n_struct, n_struct).detach().numpy()
+    min_energy = np.min(energies)
     np.savetxt("energies_structured.txt", grad_2_zz)
-    np.savetxt("energies_factor_structured.txt", grad_2_zz*np.exp(-1.0*energies))
+    np.savetxt("energies_factor_structured.txt", grad_2_zz*np.exp(-beta.values().item()*energies))
 
     from scipy.integrate import simpson
-    integral_y = simpson(grad_2_zz * np.exp(-1.0 * energies), x=points_y)  # Integrate over y
+    integral_y = simpson(0.5*grad_2_zz * np.exp(-beta.values().item() * (energies - min_energy)), x=points_y)  # Integrate over y
     integral_xy = simpson(integral_y, x=points_x)  # Integrate over y
+    print("BKE Loss (Computed via Numerical Integration): ", integral_xy)
 
     # Evaluate weighted gradient
-    points = np.zeros((5,2))
-    points[0,:] = np.array((-1.0,1.0))
-    points[1,:] = np.array((0.0, 0.5))
-    points[2,:] = np.array((react_min[0],react_min[1]))
-    points[3,:] = np.array((prod_min[0],prod_min[1]))
-    points[4,:] = np.array((-0.75,0.6))
-    for i in range(5):
-        p = Point(points[i,0],points[i,1])
-        energy_ = energy(points[i,0],points[i,1],A,a,b,c,x_,y_)
-        grad_ = Gu(p)
-        grad_2_ = grad_[0]**2+grad_[1]**2
-        print(points[i,:])
-        print(grad_2_*np.exp(-1.0*energy_))
+    # points = np.zeros((5,2))
+    # points[0,:] = np.array((-1.0,1.0))
+    # points[1,:] = np.array((0.0, 0.5))
+    # points[2,:] = np.array((react_min[0],react_min[1]))
+    # points[3,:] = np.array((prod_min[0],prod_min[1]))
+    # points[4,:] = np.array((-0.75,0.6))
+    # for i in range(5):
+    #     p = Point(points[i,0],points[i,1])
+    #     energy_ = energy(points[i,0],points[i,1],A,a,b,c,x_,y_)
+    #     grad_ = Gu(p)
+    #     grad_2_ = grad_[0]**2+grad_[1]**2
+    #     print(points[i,:])
+    #     print(grad_2_*np.exp(-1.0*energy_))
 
     #Pick out values of quantity to minimize that are of high interest
-    values = grad_2_zz*np.exp(-1.0*energies) > 20
+    values = grad_2_zz*np.exp(-beta.values().item()*energies) > 20
     values = values.astype(int)
     np.savetxt("values_of_interest.txt", values, fmt='%d')
 
