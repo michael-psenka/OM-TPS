964e90477d2ed29a0577dceef66815d0b9209632
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2025-02-21 11:07:08 -0800

    fast folder committor rate training up and running, not yet properly learning committor

--- two-for-one-diffusion/sample.sh
+++ two-for-one-diffusion/sample.sh
@@ -2,8 +2,8 @@ python sample.py \
     --model_path saved_models/trp_cage \
     --gen_mode om_interpolate \
     --post_om_md_simulate \
-    --n_timesteps 1000 \
-    --save_interval 10 \
+    --n_timesteps 100000 \
+    --save_interval 1000 \
     --num_samples_eval 2 \
     --batch_size_gen 2 \
     --latent_time 15 \
