2d0d98e8f4014af3f9db131361aadfa1a34cf43a
michael-psenka <michael.psenka@gmail.com>
2025-03-19 17:10:22 -0700

    sample script moved mpsenka testing to comments

--- two-for-one-diffusion/sample.sh
+++ two-for-one-diffusion/sample.sh
@@ -14,6 +14,12 @@
 #     --num_samples 100 \
 #     --iterations_on_val 1
 
+# source /home/mpsenka/miniconda3/etc/profile.d/conda.sh
+# conda activate moldyn
+# cd /home/mpsenka/repos/OMBasics/two-for-one-diffusion
+
+
+
 python sample.py \
     --model_path saved_models/chignolin \
     --gen_mode om_interpolate \
@@ -34,6 +40,29 @@ python sample.py \
     --path_length 100 \
     --path_batch_size 100 \
     --steps 500
+
+# python sample.py \
+#     --model_path /home/mpsenka/models \
+#     --gen_mode om_interpolate \
+#     --num_samples_eval 1 \
+#     --atom_selection protein \
+#     --num_samples_eval 4 \
+#     --batch_size_gen 1 \
+#     --latent_time 10 \
+#     --initial_guess_level 250 \
+#     --subsample_points_percent 1.0 \
+#     --subsample_dimensions_percent 1.0 \
+#     --no_encode_and_decode \
+#     --action hutch \
+#     --optimizer sgd \
+#     --lr 1e-5 \
+#     --append_exp_name chignolin_om_interpolate_test \
+#     --om_dt 0.001 \
+#     --om_gamma 1 \
+#     --path_length 4 \
+#     --path_batch_size 4 \
+#     --om_d 1.0 \
+#     --steps 1000
 
 # python main_train.py \
 #     --mol chignolin \
