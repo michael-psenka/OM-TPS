d74b0879b367768081c3ef73cb103e8fea1b303e
jeevster <sanjeevr@berkeley.edu>
2025-04-07 17:32:28 -0700

    flowmatching on escher

--- two-for-one-diffusion/submit_jobs.sh
+++ two-for-one-diffusion/submit_jobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --mail-type=BEGIN,END,FAIL  # Send email when job begins, ends, or fails
 #SBATCH --mail-user=sanjeevr@umich.edu  # Replace with your email
-#SBATCH --partition=long
-#SBATCH --qos=long
-#SBATCH --nodelist=germain
+#SBATCH --partition=scavenger
+#SBATCH --qos=scavenger
+#SBATCH --nodelist=escher
 #SBATCH --gpus=2
 #SBATCH --time=48:00:00
 
@@ -16,19 +16,20 @@ python main_train.py \
     --mol tetrapeptides \
     --atom_selection all-atom \
     --data_folder /data/sanjeevr/4AA_data  \
+    --flow_matching \
     --eval_interval 1000 \
-    --warmup_proportion 0.05 \
+    --warmup_proportion 0.15 \
     --batch_size 512 \
     --gradient_accumulate_every 1 \
     --train_iter 250000 \
     --weight_decay 0 \
     --learning_rate 4e-4 \
     --min_lr_cosine_anneal 0 \
-    --experiment_name tetra_all_atom_bs=512_weightdecay=0_trainlonger_LARGER \
+    --experiment_name tetra_all_atom_flowmatching_bs=512_weightdecay=0_trainlonger_LARGER \
     --scale_data False \
-    --gradient_norm_threshold 50 \
+    --gradient_norm_threshold 100000 \
     --num_samples 100 \
-    --start_from_last_saved True \
+    --start_from_last_saved False \
     --num_layers_gnn 4 \
     --hidden_features_gnn 256 \
 
