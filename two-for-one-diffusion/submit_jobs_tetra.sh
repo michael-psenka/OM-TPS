7eee340fe580f0b847f2c9633e79453bdc204ba5
jeevster <sanjeevr@berkeley.edu>
2025-04-17 10:11:10 -0700

    commit before switch

--- two-for-one-diffusion/submit_jobs_tetra.sh
+++ two-for-one-diffusion/submit_jobs_tetra.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 #SBATCH --mail-type=BEGIN,END,FAIL  # Send email when job begins, ends, or fails
 #SBATCH --mail-user=sanjeevr@umich.edu  # Replace with your email
-#SBATCH --partition=long
-#SBATCH --qos=long
-#SBATCH --nodelist=germain
+#SBATCH --partition=scavenger
+#SBATCH --qos=scavenger
+#SBATCH --nodelist=escher
 #SBATCH --gpus=1
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 
 source /home/sanjeevr/mambaforge/etc/profile.d/conda.sh
 conda activate alphaflow
 cd /home/sanjeevr/om-diffusion/two-for-one-diffusion
 
-
+export PYTHONPATH=./
 python sample.py \
     --model_path saved_models/tetrapeptides_all_atom \
     --sidechains \
     --flow_matching \
     --gen_mode om_interpolate \
     --data_folder /data/sanjeevr/4AA_sim \
-    --split mdgen/splits/4AA_test_1.csv \
+    --split mdgen/splits/4AA_test_2.csv \
     --num_samples_eval 16  \
     --batch_size_gen 2 \
     --latent_time 0.5 \
@@ -29,8 +29,8 @@ python sample.py \
     --action truncated \
     --optimizer adam \
     --lr 2e-1 \
-    --append_exp_name test_april14_model_fulldataset_dt=0.0002 \
+    --append_exp_name test_april14_model_fulldataset_dt=0.0001 \
     --path_length 100 \
-    --om_dt 0.0002 \
+    --om_dt 0.0001 \
     --om_gamma 1 \
     --steps 250 \
