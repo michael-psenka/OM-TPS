d397432239c8d1f7faaafc863a09181ecd501c74
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2025-05-23 15:37:32 -0700

    do some reorg

---
+++ muller-brown/mb_actions.py
@@ -0,0 +1,186 @@
+import torch
+from torch.func import vmap, grad, vjp
+
+
+class S2Action(torch.nn.Module):
+    """Action with Hessian calculated using autodiff - Hessian is exact."""
+
+    def __init__(self, force_func=None, laplace_func=None, dt=1.0, gamma=1.0, D=1.0):
+        """
+        Args:
+            force_func: Force function
+            laplace_func: Laplace function
+            dt: float, time step
+            gamma: float, diffusion coefficient
+            D: float, diffusion coefficient
+        """
+        super(S2Action, self).__init__()
+        self.force_func = vmap(force_func)
+        self.laplace_func = laplace_func
+        self.dt = dt
+        self.gamma = gamma
+        self.D = D
+
+    def forward(self, path: torch.Tensor):
+        """
+        Args: path of shape [P, 2]
+        """
+
+        x_n = path[:-1]
+        x_np = path[1:]
+        f_n = self.force_func(x_n)
+
+        first_term = torch.sum(
+            torch.square((x_np - x_n)) * (self.gamma / 4 / self.dt), axis=-1
+        )
+        second_term = torch.sum(torch.square(f_n) * (self.dt / 4 / self.gamma), axis=-1)
+
+        exact_laplace = self.laplace_func(x_n)
+
+        third_term = exact_laplace.squeeze() * (self.dt * self.D / torch.tensor(2.0))
+
+        assert (
+            first_term.shape == second_term.shape
+            and second_term.shape == third_term.shape
+        )
+        result = torch.sum(first_term + second_term - third_term)
+
+        # Result is the action and is expected to have shape [batch, 1]
+        return result
+
+
+class TruncatedAction(torch.nn.Module):
+    """S2Action with Hessian term ignored."""
+
+    def __init__(self, force_func, dt, gamma, laplace_func=None, D=None):
+        """
+        Args:
+            force_func: Force function
+            dt: float, time step
+            gamma: float, diffusion coefficient
+            D: float, diffusion coefficient
+        """
+        super(TruncatedAction, self).__init__()
+        self.force_func = force_func
+        self.dt = dt
+        self.gamma = gamma
+
+    def forward(self, path: torch.Tensor):
+        """
+        Args: path of shape [P, 2]
+        """
+        first_term = torch.square((path[1:] - path[:-1])) * (self.gamma / 4 / self.dt)
+        second_term = torch.square(self.force_func(path[:-1])) * (
+            self.dt / 4 / self.gamma
+        )
+
+        result = torch.sum(first_term + second_term)
+        # return result
+        return first_term.sum(), second_term.sum()
+
+
+class SimpleAction(torch.nn.Module):
+    """
+    Action without hessian.
+    Basically same as src.actions.SimpleAction but for 2D MB, not images.
+    """
+
+    def __init__(self, force_func, dt, gamma, laplace_func=None, D=None):
+        """
+        Args:
+            force_func: Force function
+            dt: float, time step
+            gamma: float, diffusion coefficient
+        """
+        super(SimpleAction, self).__init__()
+        self.force_func = force_func
+        self.dt = dt
+        self.gamma = gamma
+
+    def forward(self, path: torch.Tensor):
+        # Args: path of shape [P, 2]
+        first_term = torch.square((path[1:] - path[:-1])) * (self.gamma / self.dt)
+        f_n = self.force_func(path[:-1])
+        f_np = self.force_func(path[1:])
+        second_term = (torch.square(f_n) + torch.square(f_np)) * (
+            self.dt / self.gamma / 2.0
+        )
+        third_term = (path[1:] - path[:-1]) * (f_np - f_n)
+
+        result = torch.sum(first_term + second_term + third_term)
+        return result / torch.tensor(4.0)
+
+
+class HutchinsonAction(torch.nn.Module):
+    """
+    Action that is the same as S2 Action but calculates laplacian using Hutchinson trick using
+    random vector of -1s and 1s.
+    """
+
+    def __init__(
+        self,
+        dt,
+        gamma,
+        D,
+        N=1,
+        sample_force_func=None,
+        laplace_func=None,
+        force_func=None,
+        diffusion_model=True,
+    ):
+        super(HutchinsonAction, self).__init__()
+
+        sample_force_func = (
+            force_func if sample_force_func is None else sample_force_func
+        )
+
+        self.sample_force_func = sample_force_func
+        self.dt = dt
+        self.gamma = gamma
+        self.D = D
+        self.N = N
+        self.true_laplace = laplace_func
+        self.force_func = vmap(sample_force_func)
+
+        def force_and_laplace(x: torch.tensor):
+            res = 0
+
+            if diffusion_model:
+                x = x.unsqueeze(0)
+
+            forces, vjp_func = vjp(self.sample_force_func, x)
+
+            for _ in range(self.N):
+                # Generate random vector of -1s and 1s.
+                shape = x.shape
+                v = torch.randint(0, 2, shape, dtype=torch.float32) * 2 - 1
+                v = v.to(x.device)
+                # Calculate matrix vector product of Hessian and random vector
+                (Av,) = vjp_func(v)
+
+                # Make it a scalar. Minus is from the sign of the forces
+                res += -torch.sum(v * Av)
+
+            return forces, res / N
+
+        self.force_and_laplace = vmap(force_and_laplace, randomness="different")
+
+    def forward(self, path: torch.Tensor):
+
+        x_n = path[:-1]
+        x_np = path[1:]
+        f_n, laplace = self.force_and_laplace(x_n)
+
+        # Make sure the terms are [batch, 1] as is the third term
+        first_term = torch.sum(
+            torch.square((x_np - x_n)) * (self.gamma / 4 / self.dt), axis=-1
+        )
+        second_term = torch.sum(torch.square(f_n) * (self.dt / 4 / self.gamma), axis=-1)
+
+        third_term = laplace * self.dt * self.D / torch.tensor(2.0)
+
+        # First apply physical dimension sum. The sum can be rearanged but Hutch term has to be scalar. So others need to be too
+        result = torch.sum(first_term + second_term - third_term)
+
+        # Result is the action and is expected to have shape [batch, 1]
+        return result
---
+++ muller-brown/simpleMB.py
@@ -0,0 +1,172 @@
+import torch
+import numpy as np
+from torch.func import grad, grad_and_value, vmap
+
+
+class SimpleMB:
+    """
+    Simple Muller-Brown Potential to test transition path optimization.
+    """
+
+    def __init__(self, device, n_in=2, barrier=1.0):
+        """
+        Args:
+            args: Arguments object
+            n_in (int): Number of input dimensions
+            barrier (float): Barrier height
+        """
+        self.device = device
+        self.n_in = n_in
+
+        self.barrier = barrier
+        prefactor = 1e-2
+
+        barrier = 10.0 * self.barrier
+        self.A = barrier * torch.tensor([-1.73, -0.87, -1.47, 0.13])
+        self.alpha = prefactor * torch.tensor([-0.39, -0.39, -2.54, 0.273])
+        self.a = torch.tensor([48, 32, 24, 16])
+
+        self.beta = prefactor * torch.tensor([0, 0, 4.30, 0.23])
+        self.b = torch.tensor([8, 16, 32, 24])
+
+        self.gamma = prefactor * torch.tensor([-3.91, -3.91, -2.54, 0.273])
+
+        self.D = 0.0
+        self.d = 0.0
+
+        self.E = 0.0
+        self.e = 0.0
+
+        self.spring = 0.1
+
+        if self.n_in == 2:
+            self.initial_point = torch.tensor([23.0, 30.0])
+            self.final_point = torch.tensor([40.0, 9.0])
+        elif self.n_in == 5:
+            self.initial_point = torch.tensor([23.0, 25.0, 30.0, 25.0, 25.0])
+            self.final_point = torch.tensor([40.0, 25.0, 9.0, 25.0, 25.0])
+
+        self.Lx, self.Hx = -0, 50
+        self.Ly, self.Hy = 0, 50
+
+        self.U_min, self.U_max = -2 * barrier, 1 * barrier
+
+        self.total_potential = lambda X: torch.sum(self.U(X))
+
+        self.sample_force_func = lambda X: -grad_and_value(self.total_potential)(X)[0]
+
+        self.old_force_func = lambda X: (
+            torch.tensor(0),
+            -grad(self.total_potential)(X),
+        )
+
+        self.to_(device)
+
+    def to_(self, device):
+
+        self.A = self.A.to(self.device)
+        self.alpha = self.alpha.to(self.device)
+        self.a = self.a.to(self.device)
+
+        self.beta = self.beta.to(self.device)
+        self.b = self.b.to(self.device)
+        self.gamma = self.gamma.to(self.device)
+
+        # self.initial_point = self.initial_point.to(device)
+        # self.final_point = self.final_point.to(device)
+
+    def U(self, X):
+        """Simple potential that represents a transition avoiding a barrier.
+
+        Args:
+            x (Tensor): Position of the particle
+        """
+        if not torch.is_tensor(X):
+            X = torch.tensor(X, requires_grad=True)
+
+        if self.n_in == 2:
+            if len(X.shape) == 1:
+                x = X[0]
+                y = X[1]
+            else:
+                x = X[:, 0]
+                y = X[:, 1]
+            u = self.U_split(x, y)
+        else:
+            if len(X.shape) == 1:
+                x1, x2, x3, x4, x5 = torch.split(X, 1, dim=0)
+            else:
+                x1, x2, x3, x4, x5 = torch.split(X, 1, dim=-1)
+
+            x = x1
+            y = x3
+            u = self.U_split(x, y) + self.spring * (
+                (x2 - 25.0) ** 2 + (x4 - 25.0) ** 2 + (x5 - 25.0) ** 2
+            )
+        return u
+
+    def project2D(self, X):
+        x = X[..., 0]
+        y = X[..., 2]
+        projected = torch.stack((x, y), dim=-1)
+        return projected
+
+    def U_split(self, x, y):
+        """Simple potential that represents a transition avoiding a barrier for the 2D case.
+
+        Args:
+            x (Tensor): Position coordinate x
+            y (Tensor): Position coordinate y
+
+        """
+
+        u = (
+            torch.sum(
+                self.A
+                * torch.exp(
+                    self.alpha * (x.unsqueeze(-1) - self.a) ** 2
+                    + self.beta
+                    * (x.unsqueeze(-1) - self.a)
+                    * (y.unsqueeze(-1) - self.b)
+                    + self.gamma * (y.unsqueeze(-1) - self.b) ** 2
+                ),
+                axis=-1,
+            )
+            + self.D * (x - self.d) ** 3
+            - self.E * (y - self.e) ** 3
+        )
+        return u
+
+    def laplace(self, X):
+        """
+        Compute the second derivatives of the potential at a given point X.
+        I.e U_xx, U_yy. These will be added together in the action to form the laplacian.
+        """
+        assert self.n_in == 2
+        x, y = torch.split(X, 1, dim=-1)
+
+        U_xx = grad(
+            lambda x, y: torch.sum(
+                grad(lambda x, y: torch.sum(self.U_split(x, y)), argnums=(0, 1))(x, y)[
+                    0
+                ]
+            ),
+            argnums=0,
+        )(x, y)
+        U_yy = grad(
+            lambda x, y: torch.sum(
+                grad(lambda x, y: torch.sum(self.U_split(x, y)), argnums=(0, 1))(x, y)[
+                    1
+                ]
+            ),
+            argnums=1,
+        )(x, y)
+
+        return U_xx + U_yy
+
+    def get_init_point(self, batch_size):
+
+        basin1 = self.initial_point.repeat(batch_size, 1)
+        basin2 = self.final_point.repeat(batch_size, 1)
+        sim_init = torch.cat((basin1, basin2))
+        return sim_init
