3288d7b819b42b893c64b8faf94a548b59519cd2
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2025-05-28 12:57:44 -0700

    remove gcn and schnet

--- two-for-one-diffusion/models/committor.py
+++ two-for-one-diffusion/models/committor.py
@@ -19,16 +19,3 @@ class CommittorNN(torch.nn.Module):
 
         return committor_prob
 
-
-# class SimpleCommittorNN(torch.nn.Module):
-#     def __init__(self):
-#         super(CommittorNN, self).__init__()
-#         self.model = model
-#         self.sigmoid = torch.nn.Sigmoid()
-
-#     def forward(self, x, h, t):
-#         # model returns per-residue contribution to the committor
-#         per_atom_contrib = self.model(x, h, t, return_energy=True)
-#         # sum over all atoms and pass through sigmoid to get committor probability
-#         committor_prob = self.sigmoid(per_atom_contrib.sum(dim=(-2, -1)))
-#         return committor_prob
